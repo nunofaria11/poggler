df046c94d6e93822e9934e92e1d73cf89be62ca4
inesmartins <in.afmartins@gmail.com>
2015-06-30 22:58:10 +0100

    new logo

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 $('p').click(function() {
     if ($(this).attr("previous-color") === undefined) {
         $(this).attr("previous-color", $(this).css("color"));
-        $(this).css("color", "#988AE3");
+        $(this).css("color", "#46FAD0");
     } else {
         $(this).css("color", $(this).attr("previous-color"));
         $(this).removeAttr("previous-color");
